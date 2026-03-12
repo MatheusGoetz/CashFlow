40e1187e066a7ba4131bade28b70221dffd2f3b5
Matheus Goetz <goetz.dev@gmail.com>
2026-03-12 14:38:32 -0300

    refactor: refatordo código com AutoMapper

--- src/CashFlow.Domain/obj/Debug/net8.0/CashFlow.Domain.AssemblyInfo.cs
+++ src/CashFlow.Domain/obj/Debug/net8.0/CashFlow.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2af944d4c259f7ece021919e33024c257141824")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbe24d35f30e534bbcfa52345c5195e60d2509a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
